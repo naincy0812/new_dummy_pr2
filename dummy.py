a631691db5edf99ec7c292e4c388ed464a033084
naincy0812 <naincy.chourasia@apphelix.ai>
2025-05-07 16:53:47 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,2 +1,9 @@
-print(fibonacci(10))
-a, b = 1, 2
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+print(factorial(5))
+print("hello")
+a=1,b=2
