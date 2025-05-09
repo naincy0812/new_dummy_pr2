da8923b0846daf2ea074673de182c8410da07239
Naincy Chourasia <106032516+Naina0812@users.noreply.github.com>
2025-05-09 11:40:43 +0530

    Update new_dummy.py

--- new_dummy.py
+++ new_dummy.py
@@ -1,7 +1,6 @@
 def divide(a, b):
     if b == 0:
-        print("Cannot divide by zero")
-    return None
+        print("Cannot divide by zero") return None
 
-result = divide(10, 0)
-print("Result is", result)
+result = divide(10, 0
+print("Result is", result
