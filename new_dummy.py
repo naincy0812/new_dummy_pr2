0b883fcf98648b91e485ae0742cfc7e253caa78c
naincy0812 <naincy.chourasia@apphelix.ai>
2025-05-09 14:13:43 +0530

    Part of: Fix code issues in PR #35
    Fixes in new_dummy.py

--- new_dummy.py
+++ new_dummy.py
@@ -1,7 +1,7 @@
 def divide(a, b):
-    i b == 0:
+    if b == 0:
         print("Cannot divide by zero")
 return None 
 
-result = divide(10, 0)
-print("Result is", )
+reslt = divide(10, 0)
+print("Result is", reslt)
