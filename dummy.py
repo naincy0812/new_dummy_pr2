134eb1053768f81fd16b75cdb1b47112d928b4b8
naincy0812 <naincy.chourasia@apphelix.ai>
2025-05-09 12:17:04 +0530

    Part of: Fix code issues in PR #29
    Fixes in dummy.py

--- dummy.py
+++ dummy.py
@@ -1,8 +1,8 @@
 def read_file(filepath):
     file = open(filepath, 'r')
     content = file.read()
-    return content
-    file.close()
+    return content 
+    # file.close())
 
 data = read_file("data.txt")
 print(data)
