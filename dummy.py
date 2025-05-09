d8962fb9c7021cf4cab68d390252620de22a4214
Naincy Chourasia <106032516+Naina0812@users.noreply.github.com>
2025-05-09 11:40:27 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,9 +1,8 @@
 def read_file(filepath):
     file = open(filepath, 'r')
     content = file.read()
-    return content
-    finally:
-    file.close()
+    return content finally:
+    file.close(
 
 data = read_file("data.txt")
-print(data)
+print(data
