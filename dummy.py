0e6d9f094085d33fec0eb5fe074c9c921ee5e2dd
naincy0812 <naincy.chourasia@apphelix.ai>
2025-05-09 13:11:02 +0530

    Part of: Fix code issues in PR #33
    Fixes in dummy.py

--- dummy.py
+++ dummy.py
@@ -2,7 +2,7 @@ def read_file(filepath):
     file = open(filepath, 'r')
     content = file.read()
     return content 
-    file.close()
-
+    # file.close() is removed from this position
+        # print("HI") is removed from this position
 data = read_file("data.txt")
 print(data)
